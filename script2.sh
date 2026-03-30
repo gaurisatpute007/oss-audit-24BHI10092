9954efb5117207239eae5bd51172758aaa3706f5
gaurisatpute007 <gaurisatpute007@gmail.com>
2026-03-30 09:41:45 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,3 +1,21 @@
-
 #!/bin/bash
-echo "test"
+# Script 2: FOSS Package Inspector
+
+PACKAGE="python3"
+
+# Check if package is installed
+if dpkg -l | grep -q $PACKAGE; then
+    echo "$PACKAGE is installed."
+    dpkg -l | grep $PACKAGE
+else
+    echo "$PACKAGE is NOT installed."
+fi
+
+# Case statement for description
+case $PACKAGE in
+    python3) echo "Python: a powerful open-source programming language used worldwide" ;;
+    git) echo "Git: version control system created by Linus Torvalds" ;;
+    vlc) echo "VLC: open-source multimedia player" ;;
+    firefox) echo "Firefox: open-source web browser" ;;
+    *) echo "Unknown package" ;;
+esac
